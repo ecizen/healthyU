cce001cc2eebb42f29c90c464a1131e87f443b30
ecizen <hardiektatendta@gmail.com>
2024-02-07 10:08:23 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+import Slider from './component/Slider'
+import Login from './component/section/Login'
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Slider"
+          component={Slider}
+          options={{headerShown: false}}
+        />
+      <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+     
+   
   );
 }
 
